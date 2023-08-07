b6e9bc96821ddc42ba2d580a8b54731b8dcced0c
AnastasiaIvaschenko <anivasch@mail.ru>
2023-08-07 12:31:40 +0300

    fill.py was changed

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 from catalog.models import Category, Product
-import json
+# import json
 
 
 class Command(BaseCommand):
