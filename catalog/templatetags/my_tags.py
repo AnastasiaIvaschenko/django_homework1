b9864217828a61943eca058955cd94810db51f83
AnastasiaIvaschenko <anivasch@mail.ru>
2023-08-15 17:18:04 +0300

    there have been some changes with tag

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -2,7 +2,17 @@ from django import template
 
 register = template.Library()
 
+
+# Это регистрация шаблонного ФИЛЬТРА
 @register.filter
+def mediapath(image_path):
+    if image_path:
+        return f'/media/{image_path}'
+    return 'media/catalog/No_photo'
+
+
+# Это регистрация шаблонного ТЕГА
+@register.simple_tag
 def mediapath(image_path):
     if image_path:
         return f'/media/{image_path}'
